5e4f7ed6e76b8059614ce82fda8db8e5494bdd7b
Shinichi Kanehira <shinichi.kanehira@gmail.com>
2024-06-09 19:27:58 +0900

    first commit

---
+++ src/mymodule/greet.py
@@ -0,0 +1,15 @@
+def greet(name):
+    """
+    指定した名前に挨拶を返す関数
+
+    Parameters
+    ----------
+    name: str
+        挨拶をする相手の名前
+    """
+
+    # 挨拶文の作成
+    # 文字列は + で結合できる
+    greeting_text = "Hello, " + name + "!"
+
+    return greeting_text
